1c50c397a5778fc108b35564ed1e6b4f5e34b80e
RedstonePfalz <Niklas_Schommer@outlook.com>
2023-05-27 14:46:01 +0200

    CSRF-Protector hinzugefügt

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'c075d98105901f2beadf5c518b683ed11816b2e0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'c075d98105901f2beadf5c518b683ed11816b2e0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'owasp/csrf-protector-php' => array(
+            'pretty_version' => 'v1.0.2',
+            'version' => '1.0.2.0',
+            'reference' => '98b60b05b2fdc0f66fef68f51890dc437b72c3f5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../owasp/csrf-protector-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
